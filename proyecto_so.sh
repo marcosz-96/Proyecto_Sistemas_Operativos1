1a0255949bb5ba45a04c9aa4e183f414bb44feec
marcosz-96 <168491483+marcosz-96@users.noreply.github.com>
2024-11-01 18:44:35 -0300

    Update proyecto_so.sh
    Ultimos cambios adicionales

--- proyecto_so.sh
+++ proyecto_so.sh
@@ -9,66 +9,74 @@ generar_informes(){
     #Primero, definimos un archivo log, que es donde se van a guardar los informes
     archivo_log="informes_$(date +%Y%m%d_%H%M%S).log"
 
+    #Funcion adicional para agregar archivos al log
+    agregar_archivo_al_log(){
+        echo  "$1" >> $archivo_log
+        echo ""  >> $archivo_log
+    }
+
     #Luego, Obtenemos los datos de uso de la CPU
-    echo "Uso de CPU:" > "$archivo_log"
+    agregar_archivo_al_log "Uso de CPU:"
     top -b -n 1 | grep "Cpu(s)" >> "$archivo_log"
-    echo "" >> "$archivo_log"
 
     #Tambien, obtenemos datos de uso de la Memoria
-    echo "Uso de Memoria:" >> "$archivo_log"
+    agregar_archivo_al_log "Uso de Memoria:"
     free -h >> "$archivo_log"
-    echo "" >> "$archivo_log"
 
     #Y por ultimo, obtenemos datos de uso del Disco
-    echo "Uso de Disco:" >> "$archivo_log"
+    agregar_archivo_al_log "Uso de Disco:"
     df -h >> "$archivo_log"
-    echo "" >> "$archivo_log"
 
     #Enviamos mensaje de confirmación de informes hechos.
-    echo "Informes generados y guardados exitosamente en "$archivo_log"."
+    echo "Informes generados y guardados exitosamente en \"$archivo_log\"."
     echo "--------------------------------------------------------------"
     echo "Para poder ver el listado de informe, por favor
-    escriba lo siguiente 'ls -l informes_*.log'"
+    escriba lo siguiente:
+    echo "'ls -l informes_*.log'"
     echo "--------------------------------------------------------------"
-    echo "Si desea ver el informe generado, escriba lo siguiente,
-    'cat informes_$(date +%Y%m%d_%H%M%S).log'"
+    echo "Si desea ver el informe generado, escriba lo siguiente:
+    echo "'cat\"$archivo_log\"'"
 }
 
 #2da Función: Limpiar archivos temporales y caché del sistema
-#Esta función nos permite eliminar archivos temporales y caché del sistema.
+#Esta función nos permite eliminar archivos temporales del sistema y caché de los navegadores.
 
 limpiar_cache_y_archivos_temp(){
     echo "Limpiando caché y archivos temporales..."
 
     #En el primer paso de procede a eliminar los archivos temporales del sistema
-    sudo rm -rf /tem/*
-    echo "Los archivos temporales en /tem/ han sido eliminados."
+    if sudo rm -rf /tem/*; then
+        echo "Los archivos temporales en /tem/ han sido eliminados."
+    else
+        echo "No se pudo eliminar los archivos temporales en /tem/"
+    fi
 
     #lo siguiente es realizar la limpieza de los archivos caché del sistema 
-    sudo apt-get clean
-    echo "Los archivos caché APT del sistema, limpiado."
+    if sudo apt-get clean; then
+        echo "Los archivos caché APT del sistema, limpiado."
+    else 
+        echo "No se pudo limpiar los archivos caché APT del sistema."
+    fi
 
     #Luego, procedemos a la lmpieza de los navegadores
     echo "Empezando limpieza de navagadores..."
-    #Empezamos con FireFox
-    echo "Limpiando FireFox..."
-    if [ -d "$HOME/.cache/mozilla/firefox" ]; then
-        rm -rf "$HOME/.cache/mozilla/firefox/*"
-        echo "Caché del navegador FireFox eliminado con éxito."
-    else
-        echo "No se encontraron archivos caché en el navegador FireFox."
-    fi
 
-    #Seguimos con el navegador Google
-    echo "Limpiando Google Chrome..."
-    if [ -d "$HOME/.cache/google-chrome" ]; then
-        rm -rf "$HOME/.cache/google-chrome/*"
-        echo "Caché del navegador Google Chrome eliminado con éxito."
-    else
-        echo "No se encontraron archivos caché en el Google Chrome."
-    fi
+    limpiar_navegadores(){
+        navegador =$1
+        ruta_cache =$2
+        echo "limpiando navegador $navegador"
+        if [-d "$ruta_cache"]; then
+            rm -rf "$ruta_cache"
+            echo "Caché del navegador $navegador eliminado."
+        else 
+            echo "No se encontraron archivos caché del navegador $navegador."
+        fi
+    }
 
-    echo "Limpieza completada con éxito."
+echo "Empezando limpieza de navegadores"
+limpiar_navegadores "FireFox" "$HOME/.cache/mozilla/firefox"
+limpiar_navegadores "Google Chrome" "$HOME/.cache/google-chrome"
+echo "Limpieza completada con éxito."
 }
 
 #3ra Función: Actualización e instalación de paquetes
@@ -94,17 +102,25 @@ actualizar_sistema(){
         fi
 }
 
-#4ta Función: Menú Interactivo
+#4ta Función: Definición de colores
+RED ='\033[0;31m'
+GREEN ='\033[0;32m'
+YELLOW ='\033[0;33m'
+BLUE ='\033[0;34m'
+NC  ='\033[0m' # Sin Color
+
+
+#5ta Función: Menú Interactivo
 while true; do
-    echo "-----------------------------------------------------------------------"
-    echo "Bienvenido al menú de opciones"
-    echo "A continuación te presentamos las siguienbtes opciones para que puedas:"
-    echo "-----------------------------------------------------------------------"
-    echo "1. Generar un informe de uso de CPU, memoria y disco"
-    echo "2. Verificar y actualizar en sistema"
-    echo "3. Realizar una limpieza de archivos temporales y caché"
-    echo "4. Finalizar el programa"
-    echo "-----------------------------------------------------------------------"
+    echo -e"${BLUE}-----------------------------------------------------------------${NC}"
+    echo -e"${YELLOW}Bienvenido al menú de opciones${NC}"
+    echo -e"${BLUE}A continuación te presentamos las siguienbtes opciones para que puedas:${NC}"
+    echo "-------------------------------------------------------------------------------"
+    echo -e"${GREEN}1.${NC} Generar un informe de uso de CPU, memoria y disco."
+    echo -e"${GREEN}2.${NC} Verificar y actualizar en sistema."
+    echo -e"${GREEN}3.${NC} Realizar una limpieza de archivos temporales y caché."
+    echo -e"${GREEN}4.${NC} Finalizar el programa."
+    echo -e"${BLUE}-------------------------------------------------------------------${NC}"
     read -p "Elija una opción: " opcion
 
     case $opcion in
@@ -118,11 +134,11 @@ while true; do
         limpiar_cache_y_archivos_temp 
         ;;
     4) 
-        echo "Gracias por utilizar el menú de opciones"  
+        echo -e"${GREEN}Gracias por utilizar el menú de opciones${NC}"  
         break #Sale del bucle
         ;;
     *)
-        echo "Opción no válida, por favor elija una opción válida"
+        echo -e"${RED}Opción no válida, por favor elija una opción válida${NC}"
         ;;
     esac
 done
